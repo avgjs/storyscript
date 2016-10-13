8ae3f452cd13079d84248e3297b5ace5ee462d95
Icemic <bingfeng.jw@gmail.com>
2016-10-13 20:54:05 +0800

    update copyright

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/**
+/*!
  * Copyright 2016 Icemic Jia <bingfeng.web@gmail.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
   entry: "./index.js",
@@ -15,6 +16,21 @@ module.exports = {
       { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
     ]
   },
+  plugins: [
+    new webpack.BannerPlugin(`Copyright 2016 Icemic Jia <bingfeng.web@gmail.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.`),
+  ],
   devServer: {
     historyApiFallback: true,
     hot: true,
